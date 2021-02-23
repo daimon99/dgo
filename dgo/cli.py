e3fcbc4b04a25f982f8aedebc6d72730858811e7
daimon <daimon@magicbook>
2021-02-23 10:56:32 +0800

    fea: add enc/dec

--- dgo/cli.py
+++ dgo/cli.py
@@ -4,8 +4,6 @@ import sys
 import os
 import click
 import requests
-import subprocess
-
 
 @click.group()
 def main(args=None):
@@ -13,9 +11,9 @@ def main(args=None):
 
 
 @main.command()
-@click.argument('local_file_path', type=click.File('rb'))
+@click.argument(u'local_file_path', type=click.File('rb'))
 def upload(local_file_path):
-    """
+    u"""
     Temporary upload a file.
     Then can download it later.
     But not guarantee when to delete from the server.
@@ -29,16 +27,16 @@ def upload(local_file_path):
 
 
 @main.command()
-@click.argument('url')
+@click.argument(u'url')
 def wget(url):
-    """wget with usual params."""
+    u"""wget with usual params."""
     cmd = "wget --content-disposition \"%s\"" % url
     os.system(cmd)
 
 
 @main.command()
 def pipconf():
-    """创建pip 配置文件，使用国内镜像
+    u"""创建pip 配置文件，使用国内镜像
     """
     pip_conf_file_content = """
 [global]
@@ -55,10 +53,10 @@ trusted-host=
 
 @main.command()
 def pypirc():
-    """pypirc配置文件"""
+    u"""pypirc配置文件"""
     pypirc_path = os.path.expanduser('~/.pypirc')
     if os.path.exists(pypirc_path):
-        click.secho('~/.pypirc 文件已经存在', fg='red')
+        click.secho(u'~/.pypirc 文件已经存在', fg='red')
         return 1
 
     content = """
@@ -76,12 +74,12 @@ password: %s
     content = content % (username, password)
     with open(pypirc_path, 'w') as fout:
         fout.write(content)
-    click.secho('~/.pypirc 生成完毕。', fg='green')
+    click.secho(u'~/.pypirc 生成完毕。', fg='green')
 
 
 @main.command()
 def goenv():
-    """go 国内镜像"""
+    u"""go 国内镜像"""
     content = u"""
 go env -w GO111MODULE=on
 go env -w GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
@@ -90,6 +88,29 @@ go env -w GOPROXY=https://mirrors.aliyun.com/goproxy/,direct
 """
     click.secho(content, fg='cyan')
 
+
+@main.command()
+@click.option(u'--input', u'-i', u'input_path', help=u'输入文件路径')
+def enc(input_path):
+    u"""加密。如果提供了 input_path，则对指定文件加密。否则从 stdin 读数"""
+    extra = ""
+    if input_path:
+        extra = u'-in "%s" -out "%s.enc"' % (input_path, input_path)
+    salt_key = click.prompt(u'请输入Key: ', hide_input=True)
+    cmd = u'openssl aes-256-cbc -k %s -a -md md5 -base64 %s' % (salt_key, extra)
+    os.system(cmd)
+
+
+@main.command()
+@click.option(u'--input', u'-i', u'input_path', help=u'输入文件路径')
+def dec(input_path):
+    u"""解密。如果提供了 input_path，则对指定文件解密。否则从 stdin 读数"""
+    extra = ""
+    if input_path:
+        extra = u'-in "%s" -out "%s.dec"' % (input_path, input_path)
+    salt_key = click.prompt(u'请输入Key: ', hide_input=True)
+    cmd = u'openssl aes-256-cbc -k %s -a -md md5 -base64 -d %s' % (salt_key, extra)
+    os.system(cmd)
 
 if __name__ == "__main__":
     sys.exit(main())  # pragma: no cover
